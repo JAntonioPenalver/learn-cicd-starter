3b3f40da7ddab62af1edca8db7ed3f5521eddaf7
JAntonioPenalver <29joseantonio11@gmail.com>
2026-08-16 00:35:09 +0200

    update cd workflow and migration script

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-
-# Usar DATABASE_URL o DB_URL
 URL="${DATABASE_URL:-$DB_URL}"
-
 if [ -z "$URL" ]; then
   echo "Error: DATABASE_URL is not set"
   exit 1
 fi
-
-# Eliminar comillas dobles o simples que hayan quedado en la variable
 CLEAN_URL=$(echo "$URL" | tr -d '"' | tr -d "'")
-
-# Navegar a la carpeta donde están los archivos .sql
 cd sql/schema || exit 1
-
-# Ejecutar las migraciones
 goose turso "$CLEAN_URL" up
